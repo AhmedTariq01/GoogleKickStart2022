887dedacc37a2fc6579f75207fe0dc6f56e9f44f
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-03-29 14:15:07 +0500

    Palindrome Free Strings

--- GoogleKickStart 2020 Round A/PalindromeFreeStrings.py
+++ GoogleKickStart 2020 Round A/PalindromeFreeStrings.py
@@ -40,4 +40,36 @@
 
 # Solution
 
+# Google Kick Start 2022 Round A - Problem C. Palindrome Free Strings
+#
 
+
+def check(s):
+    return len(s) <= 4 or s != s[::-1]
+
+
+def iter_backtracking(s):
+    lookup = {i for i, x in enumerate(s) if x == '?'}
+    stk = []
+    i = 0
+    while i < len(s):
+        if i in lookup:
+            s[i] = '0'
+            stk.append(i)
+        while not (check(s[i - 4:i + 1]) and check(s[i - 5:i + 1])):
+            if not stk:
+                return False
+            i = stk.pop()
+            s[i] = '1'
+        i += 1
+    return True
+
+
+def palindrome_free_strings():
+    N = int(input().strip())
+    S = list(input().strip())
+    return "POSSIBLE" if iter_backtracking(S) else "IMPOSSIBLE"
+
+
+for case in range(int(input())):
+    print("Case #{}: {}".format(case + 1, palindrome_free_strings()))
